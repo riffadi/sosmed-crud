f3b4a8db3e68f9f973832756c15391d34c730258
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-09-01 16:30:19 +0700

    create models instagram

--- sosmed/models.py
+++ sosmed/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Instagram(models.Model):
 	nama_depan = models.CharField(max_length=100)
 	nama_belakang = models.CharField(max_length=100)
-
+	username = models.CharField(max_length=100)
 
 	def __str__(self):
 		return "{}.{}".format(self.id, self.username)
